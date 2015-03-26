5769e24cca004464c88b60dc8551efb677999e77
eceeb <dev.mail@icloud.com>
2015-03-26 15:38:42 +0100

    Rename sys-property

--- src/main/java/de/eliyo/service/SearchService.java
+++ src/main/java/de/eliyo/service/SearchService.java
@@ -43,7 +43,7 @@ public class SearchService {
 	
 
 	private Connection getConnection() throws Exception {
-		URI dbUri = new URI(System.getenv("DB_URL"));
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath();

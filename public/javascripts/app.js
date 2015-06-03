3c1a055d298d0b81f638374d1579fbca7873fd49
Eceeb <dev.mail@icloud.com>
2015-06-03 22:02:03 +0200

    Convert project to Node.js

---
+++ public/javascripts/app.js
@@ -0,0 +1,43 @@
+angular.module('search', [])
+
+.controller('searchController', function($scope, $http) {
+
+    $scope.formData = {};
+    $scope.todoData = {};
+    $scope.formData.interval = 10;
+	var alertBadge = 'collapse';
+	var alertMessage = '';
+		
+	// Create a new search entry
+	$scope.insertSearch = function(todoID) {
+	
+		// some clients don't allow client side validation
+		var search = $scope.formData;
+		if (!search.url || !search.seek || !search.email)
+			return;
+		
+		$http.post('/api/v1/todos', $scope.formData)
+			.success(function(data) {
+				var s = $scope.formData;
+				alertMessage = 'Successfully inserted search: ' + s.seek + ' on: ' + s.url;
+				alertBadge  = 'alert-success';
+				$scope.formData.url = '';
+				$scope.formData.seek = '';
+				$scope.todoData = data;
+				console.log(data);
+			})
+			.error(function(error) {
+				alertBadge = 'alert-danger';
+				alertMessage = 'Sorry something went wrong please try again';
+		});
+	};
+	
+	$scope.getAlertBadge = function() {
+		return alertBadge;
+	}
+	
+	$scope.getAlertMessage = function() {
+		return alertMessage;
+	}
+
+});
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var pg = require('pg');
+var connectionString = process.env.DATABASE_URL;
+
+router.get('/', function(req, res, next) {
+	res.sendFile(path.join(__dirname, '../views', 'index.html'));
+});
+
+router.post('/api/v1/todos', function(req, res) {
+
+	// Grab data from http request
+	var data = {url: req.body.url, seek: req.body.seek, email: req.body.email, interval: req.body.interval};
+	
+	// Get Postgres client from connection pool
+	pg.connect(connectionString, function(error, client, done) {
+		
+		if (error){
+			res.statusCode = 505;
+			return res.json();
+		}
+		
+		var insert = 'insert into wanted (url, seek, email, interval) values ($1, $2, $3, $4);'
+		client.query(insert, [data.url, data.seek, data.email, data.interval]);
+		
+		client.end();
+		console.log("Inserted new search");
+		return res.json();
+	});
+});
+
+module.exports = router;
